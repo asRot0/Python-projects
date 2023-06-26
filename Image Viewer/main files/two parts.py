0b0b4f4154a126a9ac2542b25f7836c1791801f3
asif <asahmed8890@gmail.com>
2023-06-26 20:15:26 +0600

    Add files via upload

--- Image Viewer/main files/two parts.py
+++ Image Viewer/main files/two parts.py
@@ -3,19 +3,19 @@ from tkinter import Tk, Frame, Label
 def create_widgets(root):
     # Create a frame for the left part
     left_frame = Frame(root, width=200, height=200)
-    left_frame.grid(row=0, column=0, padx=10, pady=10)
+    left_frame.pack(side="left", padx=10, pady=10)
 
     # Create a label in the left frame
     left_label = Label(left_frame, text="Left Part")
-    left_label.pack(side="left")
+    left_label.pack(anchor="w")
 
     # Create a frame for the right part
     right_frame = Frame(root, width=200, height=200)
-    right_frame.grid(row=0, column=1, padx=10, pady=10)
+    right_frame.pack(side="right", padx=10, pady=10)
 
     # Create a label in the right frame
     right_label = Label(right_frame, text="Right Part")
-    right_label.pack(side="right")
+    right_label.pack(anchor="e")
 
 def main():
     # Create the Tkinter root window
@@ -23,7 +23,10 @@ def main():
 
     # Set the window title
     root.title("Divided Window")
-    root.geometry('400x400')
+
+    # Set the window size
+    root.geometry("500x300")
+
     # Call the function to create the widgets
     create_widgets(root)
 
