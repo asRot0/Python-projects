268e93f3f6baedb03bdc3aa79b79389ea4db5752
asif <asahmed8890@gmail.com>
2023-06-27 21:13:10 +0600

    Add files via upload

--- Image Viewer/main files/dirimg.py
+++ Image Viewer/main files/dirimg.py
@@ -2,66 +2,84 @@ import os
 from tkinter import Tk, Frame, Label, Button, filedialog
 from PIL import ImageTk, Image
 
-
 class ImageViewer:
     def __init__(self, root):
         self.root = root
         self.root.title("Image Viewer")
 
-        # Create a frame to hold the image
-        self.image_frame = Frame(self.root)
-        self.image_frame.pack()
+        # Create a frame to hold the image display
+        self.image_frame = Frame(self.root, bg="lightgray")
+        self.image_frame.pack(side="left", padx=10, pady=10, fill="both", expand=True)
 
         # Create a label to display the image
         self.image_label = Label(self.image_frame)
-        self.image_label.pack()
+        self.image_label.pack(fill="both", expand=True)
 
         # Create buttons for image navigation
-        self.prev_button = Button(self.root, text="Previous", command=self.load_previous_image)
-        self.prev_button.pack(side="left")
+        self.prev_button = Button(self.image_frame, text="Previous", command=self.load_previous_image)
+        self.prev_button.pack(side="left", padx=5, pady=5)
 
-        self.next_button = Button(self.root, text="Next", command=self.load_next_image)
-        self.next_button.pack(side="right")
+        self.next_button = Button(self.image_frame, text="Next", command=self.load_next_image)
+        self.next_button.pack(side="right", padx=5, pady=5)
 
         # Create a button to open the file dialog
-        self.open_button = Button(self.root, text="Open Image", command=self.open_image)
-        self.open_button.pack()
+        self.open_button = Button(self.image_frame, text="Open Image", command=self.open_image)
+        self.open_button.pack(padx=5, pady=5)
+
+        # Create a button to save the image
+        self.save_button = Button(self.image_frame, text="Save Image", command=self.save_image)
+        self.save_button.pack(padx=5, pady=5)
 
         # Initialize variables
         self.images = []
         self.image_index = 0
 
     def open_image(self):
-        # Open a file dialog to select a directory
-        directory = filedialog.askdirectory()
+        # Open a file dialog to select an image file
+        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png *.gif")])
 
-        if directory:
-            # Get a list of image files in the selected directory
-            image_files = [file for file in os.listdir(directory) if
-                           file.lower().endswith(('.jpg', '.jpeg', '.png', '.gif'))]
+        if file_path:
+            # Load the image using PIL
+            image = Image.open(file_path)
 
-            if image_files:
-                # Sort the image files alphabetically
-                image_files.sort()
+            # Calculate the new size based on 80% of the root window size
+            root_width = self.root.winfo_width()
+            root_height = self.root.winfo_height()
+            new_width = int(root_width * 0.8)
+            new_height = int(root_height * 0.8)
 
-                # Load and store the images
-                self.images = []
-                for file in image_files:
-                    # Construct the full file path
-                    file_path = os.path.join(directory, file)
+            # Resize the image
+            image = image.resize((new_width, new_height))
 
-                    # Load the image using PIL
-                    image = Image.open(file_path)
-                    image.thumbnail((800, 800))  # Resize the image for display
+            # Convert the PIL image to Tkinter PhotoImage
+            image_tk = ImageTk.PhotoImage(image)
 
-                    # Convert the PIL image to Tkinter PhotoImage
-                    image_tk = ImageTk.PhotoImage(image)
+            # Update the image label
+            self.image_label.configure(image=image_tk)
+            self.image_label.image = image_tk
 
-                    # Store the file path and PhotoImage in the list
-                    self.images.append((file_path, image_tk))
+    def save_image(self):
+        if self.images:
+            # Get the current image file path
+            file_path, _ = self.images[self.image_index]
 
-                # Display the first image
-                self.load_image()
+            # Open the file dialog to select a save location
+            save_path = filedialog.asksaveasfilename(defaultextension=".png")
+
+            if save_path:
+                # Save the image
+                image = Image.open(file_path)
+                image.save(save_path)
+
+    def load_previous_image(self):
+        if self.image_index > 0:
+            self.image_index -= 1
+            self.load_image()
+
+    def load_next_image(self):
+        if self.image_index < len(self.images) - 1:
+            self.image_index += 1
+            self.load_image()
 
     def load_image(self):
         if self.images:
@@ -79,22 +97,11 @@ class ImageViewer:
             self.prev_button.config(state="normal" if self.image_index > 0 else "disabled")
             self.next_button.config(state="normal" if self.image_index < len(self.images) - 1 else "disabled")
 
-    def load_previous_image(self):
-        if self.image_index > 0:
-            self.image_index -= 1
-            self.load_image()
-
-    def load_next_image(self):
-        if self.image_index < len(self.images) - 1:
-            self.image_index += 1
-            self.load_image()
-
-
 # Create the Tkinter root window
-window = Tk()
+root = Tk()
 
 # Create the image viewer instance
-image_viewer = ImageViewer(window)
+image_viewer = ImageViewer(root)
 
 # Start the Tkinter event loop
-window.mainloop()
+root.mainloop()
--- Image Viewer/main files/test.py
+++ Image Viewer/main files/test.py
@@ -9,47 +9,52 @@ class ImageViewer:
         self.root.title("Image Viewer")
 
         # Create a frame to hold the next and previous button
-        self.image_button_frame = Frame(self.root, bg='red')
+        self.image_button_frame = Frame(self.root, bg='#CFCFCF')
         self.image_button_frame.grid(row=0, column=0, sticky='w', pady=1)
 
         # Create a frame to hold the image display
-        self.image_frame = Frame(self.root, bg="lightgray")
+        self.image_frame = Frame(self.root, bg='#9B9C9C')
         self.image_frame.grid(row=1, column=0, sticky='w')
 
         # Create a label to display the image
-        self.image_label = Label(self.image_frame)
-        self.image_label.pack(side='left')
+        self.image_label = Label(self.image_frame, bg='#9B9C9C')
+        self.image_label.pack(fill='both', expand=True)
 
         # Create a frame for the editing section
-        self.edit_frame = Frame(self.root, bg="lightblue")
+        self.edit_frame = Frame(self.root, bg='#CFCFCF')
         self.edit_frame.grid(row=0, column=1, rowspan=1, sticky='e')
 
         # Create a frame2 for the editing section
-        self.edit_frame2 = Frame(self.root, bg="lightblue")
+        self.edit_frame2 = Frame(self.root, bg='#CFCFCF')
         self.edit_frame2.grid(row=1, column=1, rowspan=1, sticky='en')
 
         # Create buttons for image navigation
-        self.prev_button = Button(self.image_button_frame, text="Previous", command=self.load_previous_image, width=40)
+        self.prev_button = Button(self.image_button_frame, text="Previous", command=self.load_previous_image,
+                                  width=40, bg='#BDBFBF')
         self.prev_button.pack(side="left", padx=5, pady=5)
 
-        self.next_button = Button(self.image_button_frame, text="Next", command=self.load_next_image, width=40)
+        self.next_button = Button(self.image_button_frame, text="Next", command=self.load_next_image,
+                                  width=40, bg='#BDBFBF')
         self.next_button.pack(side="right", padx=5, pady=5)
 
         # Create a button to open the file dialog
-        self.open_button = Button(self.edit_frame, text="Open Image", command=self.open_image)
+        self.open_button = Button(self.edit_frame, text="Open Image", command=self.open_image, bg='#BDBFBF')
         self.open_button.pack(side='right', padx=5, pady=5)
 
         # Create a button to delete the current image
-        self.delete_button = Button(self.edit_frame, text="Delete Image", command=self.delete_image)
+        self.delete_button = Button(self.edit_frame, text="Delete Image", command=self.delete_image,
+                                    bg='#BDBFBF')
         self.delete_button.pack(side='right', padx=5, pady=5)
 
-        self.edit_button = Button(self.edit_frame2, text="Edit", command=self.edit_image, width=10)
+        self.edit_button = Button(self.edit_frame2, text="Edit", command=self.edit_image,
+                                  width=10, bg='#BDBFBF')
         self.edit_button.pack(side='top', padx=5, pady=5)
         self.edit_button.config(state='disabled')
 
-        self.saveimage_button = Button(self.edit_frame2, text="Save Image", command=self.save_image, width=10)
-        self.saveimage_button.pack(side='bottom', padx=5, pady=5)
-        self.saveimage_button.config(state='disabled')
+        self.save_image_button = Button(self.edit_frame2, text="Save Image", command=self.save_image,
+                                        width=10, bg='#BDBFBF')
+        self.save_image_button.pack(side='bottom', padx=5, pady=5)
+        self.save_image_button.config(state='disabled')
 
         # Initialize variables
         self.images = []
@@ -110,7 +115,7 @@ class ImageViewer:
 
             # Enable the edit/save buttons
             self.edit_button.config(state="normal")
-            self.saveimage_button.config(state="normal")
+            self.save_image_button.config(state="normal")
 
     def load_previous_image(self):
         if self.image_index > 0:
@@ -152,7 +157,17 @@ class ImageViewer:
         pass
 
     def save_image(self):
-        print('image saved')
+        if self.images:
+            # Get the current image file path
+            file_path, _ = self.images[self.image_index]
+
+            # Open the file dialog to select a save location
+            save_path = filedialog.asksaveasfilename(defaultextension=".png")
+
+            if save_path:
+                # Save the image
+                image = Image.open(file_path)
+                image.save(save_path)
 
 # Create the Tkinter root window
 root = Tk()
@@ -160,6 +175,7 @@ root = Tk()
 # Set the window size
 root.geometry('1000x500')
 root.resizable(False, False)
+root.config(bg='#9B9C9C')
 
 # Configure grid row and column weights
 root.grid_rowconfigure(0, weight=0)
