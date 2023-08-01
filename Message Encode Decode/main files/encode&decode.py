8782377d03421fbdc15ed61ced0011ffb5bc08fb
asif <asahmed8890@gmail.com>
2023-08-01 22:44:50 +0600

    Add files via upload

--- Message Encode Decode/main files/encode&decode.py
+++ Message Encode Decode/main files/encode&decode.py
@@ -14,15 +14,15 @@ root = Tk()
 
 # defining size of window
 root.geometry("1200x800")
-
+root.config(bg='#E7F7F7')
 # setting up the title of window
 root.title("Message Encryption and Decryption")
 
-Tops = Frame(root, width=1600, relief=SUNKEN)
+Tops = Frame(root, width=1600, relief=SUNKEN, bg='#E7F7F7')
 Tops.pack(side=TOP)
 
 f1 = Frame(root, width=800, height=700,
-           relief=SUNKEN)
+           relief=SUNKEN, bg='#E7F7F7')
 f1.pack(side=LEFT)
 
 
@@ -40,11 +40,12 @@ font_style = ('Georgia', 30, 'bold')
 italic_font_style = ('Georgia', 20, 'italic')
 
 # Create the first label with bold style
-lblInfo_main = Label(Tops, font=font_style, text="SECRET MESSAGING")
+lblInfo_main = Label(Tops, font=font_style, text="SECRET MESSAGING", bg='#E7F7F7')
 lblInfo_main.grid(row=0, column=0)
 
 # Create the second label with italic style
-lblInfo_vigenere = Label(Tops, font=italic_font_style, text="Vigenère cipher", fg="Dark Orange", bd=5, anchor='w')
+lblInfo_vigenere = Label(Tops, font=italic_font_style, text="Vigenère cipher",
+                         fg="Dark Orange", bg='#E7F7F7', bd=5, anchor='w')
 lblInfo_vigenere.grid(row=1, column=0)
 
 # Adjust column weights to align the two labels
@@ -53,7 +54,7 @@ Tops.columnconfigure(1, weight=1)
 
 lblInfo = Label(Tops, font=('arial', 20, 'bold'),
                 text=localtime, fg="Steel Blue",
-                bd=10, anchor='w')
+                bg='#E7F7F7', bd=10, anchor='w')
 
 lblInfo.grid(row=2, column=0)
 
@@ -77,7 +78,6 @@ def Reset():
 
 
 def copy_text():
-    print('get')
     copy.config(text='pasted', state='disabled')
     copytxt = txtService.get('1.0', 'end-1c')  # Retrieve the entire content of the Text widget
 
@@ -91,15 +91,15 @@ def copy_text():
 
 # labels
 lblMsg = Label(f1, font=('arial', 16, 'bold'),
-               text="MESSAGE", bd=16, anchor="w")
+               text="MESSAGE", bd=16, anchor="w", bg='#E7F7F7')
 
 lblMsg.grid(row=1, column=0)
 
-txtMsg = Text(f1, font=('arial', 10, 'bold'), wrap='word', width=35, height=5)
+txtMsg = Text(f1, font=('arial', 10, 'bold'), wrap='word', width=35, height=5, bg='#F5FEFE')
 txtMsg.grid(row=1, column=1)
 
 lblkey = Label(f1, font=('arial', 16, 'bold'),
-               text="KEY", bd=16, anchor="w")
+               text="KEY", bd=16, anchor="w", bg='#E7F7F7')
 
 lblkey.grid(row=2, column=0)
 
@@ -112,8 +112,8 @@ txtkey = Entry(f1, font=('arial', 16, 'bold'),
 txtkey.grid(row=2, column=1)
 
 lblmode = Label(f1, font=('arial', 16, 'bold'),
-                text="MODE(e for encrypt, d for decrypt)",
-                bd=16, anchor="w")
+                text="Select Mode: Encrypt or Decrypt",
+                bd=16, anchor="w", bg='#E7F7F7')
 
 lblmode.grid(row=3, column=0)
 
@@ -122,24 +122,24 @@ mode_var = StringVar()  # Create a StringVar to store the selected mode
 mode_var.set("e")  # Set the default mode to "e" (encrypt)
 
 encrypt_radio = Radiobutton(f1, font=('arial', 10, 'bold'), text="Encrypt",
-                            variable=mode_var, value="e")
+                            variable=mode_var, value="e", bg='#E7F7F7')
 encrypt_radio.grid(row=3, column=1)
 
 decrypt_radio = Radiobutton(f1, font=('arial', 10, 'bold'), text="Decrypt",
-                            variable=mode_var, value="d")
+                            variable=mode_var, value="d", bg='#E7F7F7')
 decrypt_radio.grid(row=3, column=1, columnspan=2)
 
 copy = Button(f1, font=('arial', 14),
-              text='copy', bd=0, command=copy_text, state='disabled')
+              text='copy', bd=0, command=copy_text, state='disabled', bg='#E7F7F7')
 copy.grid(row=0, column=3, sticky='e')
 
 lblService = Label(f1, font=('arial', 16, 'bold'),
-                   text="The Result-", bd=16, anchor="w")
+                   text="The Result-", bd=16, anchor="w", bg='#E7F7F7')
 
 lblService.grid(row=2, column=2)
 
 txtService = Text(f1, font=('arial', 10, 'bold'), wrap='word', width=35, height=10,
-                  state='disabled')
+                  state='disabled', bg='#F5FEFE')
 
 txtService.grid(row=1, column=3, rowspan=3)
 
