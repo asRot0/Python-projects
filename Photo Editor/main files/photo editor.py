e468b8419a35d2add9feb3910945095c323ff125
asif <asahmed8890@gmail.com>
2023-11-10 10:48:17 +0600

    Add files via upload

--- Photo Editor/main files/photo editor.py
+++ Photo Editor/main files/photo editor.py
@@ -1,7 +1,7 @@
 import customtkinter as ctk
 from image_widgets import ImageImport, ImageOutput, CloseOutput
 from menu import Menu
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageOps
 import settings
 
 
@@ -19,6 +19,14 @@ class App(ctk.CTk):
         self.image_tk = None
         self.image_output = None
 
+        self.init_parameters()
+
+        # data
+        self.image_width = 0
+        self.image_height = 0
+        self.canvas_width = 0
+        self.canvas_height = 0
+
         # layout
         self.rowconfigure(0, weight=1, uniform='a')
         self.columnconfigure(0, weight=2, uniform='a')
@@ -31,35 +39,57 @@ class App(ctk.CTk):
         self.mainloop()
 
     def init_parameters(self):
-        self.ratate_float = ctk.DoubleVar(value=settings.ROTATE_DEFAULT)
+        # self.rotate_int = ctk.DoubleVar(value=settings.ROTATE_DEFAULT)
+        self.rotate_int = ctk.IntVar(value=settings.ROTATE_DEFAULT)
+        self.zoom_int = ctk.IntVar(value=settings.ZOOM_DEFAULT)
 
+        self.rotate_int.trace('w', self.manipulate_image)
+        self.zoom_int.trace('w', self.manipulate_image)
 
+    def manipulate_image(self, *args):
+        self.image = self.original
+
+        # rotate
+        self.image = self.image.rotate(self.rotate_int.get())
+
+        # zoom
+        self.image = ImageOps.crop(image=self.image, border=self.zoom_int.get())
+
+        self.place_image()
 
     def import_image(self, path):
-        self.image = Image.open(path)
+        self.original = Image.open(path)
+        self.image = self.original
         self.image_ratio = self.image.size[0] / self.image.size[1]
         self.image_tk = ImageTk.PhotoImage(self.image)
 
         self.image_import.grid_remove()
         self.image_output = ImageOutput(self, self.resize_image)
         self.close_button = CloseOutput(self, self.close_edit)
-        self.menu = Menu(self)
+        self.menu = Menu(self, self.rotate_int, self.zoom_int)
 
     def resize_image(self, event):
         canvas_ratio = event.width / event.height
 
+        # update canvas attributes
+        self.canvas_width = event.width
+        self.canvas_height = event.height
         # resize
         if canvas_ratio > self.image_ratio:
-            image_height = int(event.height)
-            image_width = int(image_height * self.image_ratio)
+            self.image_height = int(event.height)
+            self.image_width = int(self.image_height * self.image_ratio)
         else:
-            image_width = int(event.width)
-            image_height = int(image_width / self.image_ratio)
+            self.image_width = int(event.width)
+            self.image_height = int(self.image_width / self.image_ratio)
 
+        self.place_image()
+
+    def place_image(self):
+        # place image
         self.image_output.delete('all')
-        resized_image = self.image.resize((image_width, image_height))
+        resized_image = self.image.resize((self.image_width, self.image_height))
         self.image_tk = ImageTk.PhotoImage(resized_image)
-        self.image_output.create_image(event.width / 2, event.height / 2, image=self.image_tk)
+        self.image_output.create_image(self.canvas_width / 2, self.canvas_height / 2, image=self.image_tk)
 
     def close_edit(self):
         self.image_output.grid_forget()
