3b6cf5e8d06a22346499b56d4dbf117ae174c556
asif <asahmed8890@gmail.com>
2023-11-10 19:31:18 +0600

    Add files via upload

--- Photo Editor/main files/menu.py
+++ Photo Editor/main files/menu.py
@@ -1,9 +1,10 @@
 import customtkinter as ctk
-from panels import SliderPanel
+from panels import SliderPanel, SegmentedPanel, SwitchPanel, DropDownPanel, RevertButton
+import settings
 
 
 class Menu(ctk.CTkTabview):
-    def __init__(self, parent, rotation, zoom):
+    def __init__(self, parent, pos_vars, color_vars, effect_vars):
         super().__init__(master=parent)
         self.grid(row=0, column=0, sticky='nsew', padx=5, pady=10)
 
@@ -14,20 +15,46 @@ class Menu(ctk.CTkTabview):
         self.add('Export')
 
         # widgets
-        PositionFrame(self.tab('Position'), rotation, zoom)
-        ColorFrame(self.tab('Color'))
+        PositionFrame(self.tab('Position'), pos_vars)
+        ColorFrame(self.tab('Color'), color_vars)
+        EffectFrame(self.tab('Effects'), effect_vars)
 
 
 class PositionFrame(ctk.CTkFrame):
-    def __init__(self, parent, rotation, zoom):
+    def __init__(self, parent, pos_vars):
         super().__init__(master=parent, fg_color='transparent')
         self.pack(expand=True, fill='both')
 
-        SliderPanel(self, 'Rotation', rotation, 0, 360)
-        SliderPanel(self, 'Zoom', zoom, 0, 200)
+        SliderPanel(self, 'Rotation', pos_vars['rotate'], 0, 360)
+        SliderPanel(self, 'Zoom', pos_vars['zoom'], 0, 200)
+        SegmentedPanel(self, 'Invert', pos_vars['flip'], settings.FLIP_OPTIONS)
+        RevertButton(self, (pos_vars['rotate'], settings.ROTATE_DEFAULT),
+                     (pos_vars['zoom'], settings.ZOOM_DEFAULT),
+                     (pos_vars['flip'], settings.FLIP_OPTIONS[0]))
 
 
 class ColorFrame(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, color_vars):
         super().__init__(master=parent, fg_color='transparent')
         self.pack(expand=True, fill='both')
+
+        SwitchPanel(self, (color_vars['grayscale'], 'B/W'), (color_vars['invert'], 'Invert'))
+        SliderPanel(self, 'Brightness', color_vars['brightness'], 0, 5)
+        SliderPanel(self, 'Vibrance', color_vars['vibrance'], 0, 5)
+        RevertButton(self, (color_vars['brightness'], settings.BRIGHTNESS_DEFAULT),
+                     (color_vars['grayscale'], settings.GRAYSCALE_DEFAULT),
+                     (color_vars['invert'], settings.INVERT_DEFAULT),
+                     (color_vars['vibrance'], settings.VIBRANCE_DEFAULT))
+
+
+class EffectFrame(ctk.CTkFrame):
+    def __init__(self, parent, effect_vars):
+        super().__init__(master=parent, fg_color='transparent')
+        self.pack(expand=True, fill='both')
+
+        DropDownPanel(self, effect_vars['effect'], settings.EFFECT_OPTIONS)
+        SliderPanel(self, 'Blur', effect_vars['blur'], 0, 10)
+        SliderPanel(self, 'Contrast', effect_vars['contrast'], 0, 10)
+        RevertButton(self, (effect_vars['blur'], settings.BLUR_DEFAULT),
+                     (effect_vars['contrast'], settings.CONTRAST_DEFAULT),
+                     (effect_vars['effect'], settings.EFFECT_OPTIONS[0]))
